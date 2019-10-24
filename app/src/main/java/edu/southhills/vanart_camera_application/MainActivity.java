569d75061c617fcfea7da6cddf96d49096df84be
Alan Van Art <alanv73@gmail.com>
2019-10-24 14:47:17 -0400

    initial commit

---
+++ app/src/main/java/edu/southhills/vanart_camera_application/MainActivity.java
@@ -0,0 +1,159 @@
+package edu.southhills.vanart_camera_application;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 2;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        ImageView mImageView = findViewById(R.id.ivThumbNail);
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+        if(requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK){
+            System.out.println("Completed and Saved");
+            galleryAddPic();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView ivThumbNail = findViewById(R.id.ivThumbNail);
+        ivThumbNail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent gotoGalleryIntent = new Intent(Intent.ACTION_VIEW);
+                gotoGalleryIntent.setType("image/*");
+                startActivity(gotoGalleryIntent);
+
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == REQUEST_TAKE_PHOTO){
+            // received permission result for writing to storage
+
+            // check if the only required permission has been granted
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                // write to storage permission has been granted, we can save the image
+
+                dispatchTakePictureIntentAndSave();
+
+            } else {
+                // write to storage permission was denied, can't do it
+                Toast.makeText(this,"Permission was denied",Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public void dispatchTakePictureIntent(View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    public void dispatchTakePictureIntentAndSave(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+            // create the file where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            }catch (IOException ex){
+                ex.printStackTrace();
+            }
+            // continue only if the file was successfully created
+            if(photoFile != null){
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "edu.southhills.vanart_camera_application",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    public void takeAndSaveButton(View v){
+        if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "Already Granted", Toast.LENGTH_SHORT).show();
+
+            dispatchTakePictureIntentAndSave();
+
+        } else {
+            if(shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                Toast.makeText(this,
+                        "External Storage Write Permission Required to Save Image to Gallery",
+                        Toast.LENGTH_LONG).show();
+            }
+
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_TAKE_PHOTO);
+        }
+
+    }
+
+    public File createImageFile() throws IOException{
+        // create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir   /* directory */
+        );
+
+        // save a file path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic(){
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+}
